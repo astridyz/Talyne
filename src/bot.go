84e9dbe6bc2de6f95d272adbe3ad7defb0912295
Astrid <sakurasylmere@gmail.com>
2024-05-02 17:55:49 -0300

    Bot is online

---
+++ src/bot.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"log"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func waitUntilInterrupted() {
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
+
+func main() {
+
+	log.SetFlags(0)
+	godotenv.Load("../config.env")
+
+	client, error := discordgo.New("Bot " + os.Getenv("TOKEN"))
+	if error != nil {
+		log.Fatalf("Error creating discord session: %v\n", error)
+		return
+	}
+
+	defer waitUntilInterrupted()
+
+	log.Println("Session created")
+
+	error = client.Open()
+	if error != nil {
+		log.Fatalf("Error opening connection: %v\n", error)
+		return
+	}
+
+	log.Println("Bot is online!")
+}
